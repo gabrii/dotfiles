f87f7943b241360f57f060ccb4276f35f8f266c2
Gabriel Gavilan <ggavilan@redpoints.com>
2019-01-01 21:05:08 +0100

    Added i3-gaps + i3wm-themer

---
+++ scripts/i3-gaps.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev \
+libxcb-util0-dev libxcb-icccm4-dev libyajl-dev \
+libstartup-notification0-dev libxcb-randr0-dev \
+libev-dev libxcb-cursor-dev libxcb-xinerama0-dev \
+libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev \
+autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev -y
+
+cd ..
+mkdir -p etc
+cd etc
+# clone the repository
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+
+# compile & install
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+
+# Disabling sanitizers is important for release versions!
+# The prefix and sysconfdir are, obviously, dependent on the distribution.
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
+
+cd ../../
+git clone https://github.com/unix121/i3wm-themer
+cd i3wm-themer/
+sudo pip install -r requirements.txt
+./install_ubuntu.sh
+cp -r scripts/* /home/$USER/.config/polybar/
+mkdir ~/Backups -p
+cd src
+python i3wm-themer.py --config config.yaml --backup /home/$USER/Backups
+python i3wm-themer.py --config config.yaml --install defaults/
